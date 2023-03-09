ad2e760993486575dda7b3f50a636899470b3ec2
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-03-09 16:50:37 +0000

    added normalize.css, started js. Will complete and remake in feature

---
+++ script.js
@@ -0,0 +1,35 @@
+let toggleQuestion = document.querySelector('.questionContainer');
+let content = document.querySelector('.content');
+
+// toggleQuestion.addEventListener("click", () => {
+//     return content.style.display = "block";
+// })
+
+//
+// console.log(toggleQuestion)
+// console.log(content)
+
+// toggleQuestion.addEventListener("click", showContent);
+// function showContent() {
+//     return content.style.display = "block";
+// }
+// showContent();
+// document.querySelectorAll('.questionContainer').addEventListener("click", showContent);
+//
+// function showContent() {
+//     document.querySelectorAll('.content').style.display = "block";
+// }
+
+// const showContent = () => {
+//     // if (content.style.display === "none") {
+//     //     return content.style.display = "block";
+//     // }
+//    return alert("clicked")
+// }
+//
+// toggleQuestion.addEventListener("click", (showContent));
+
+// let toggle = document.querySelector('.questionContainer')
+// toggle.addEventListener('click', () => {
+//
+// })
