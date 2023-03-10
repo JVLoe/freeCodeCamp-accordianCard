85ad5c3c9d7d1a18ff30ee0f584823a93eb44473
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-03-10 10:22:15 +0000

    added working js, everything is still a mess. e.g. css not working as intended, old files for reference

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
-let toggleQuestion = document.querySelector('.questionContainer');
-let content = document.querySelector('.content');
+const questions = document.querySelectorAll('.questionContainer');
 
-// toggleQuestion.addEventListener("click", () => {
-//     return content.style.display = "block";
-// })
-
-//
-// console.log(toggleQuestion)
-// console.log(content)
-
-// toggleQuestion.addEventListener("click", showContent);
-// function showContent() {
-//     return content.style.display = "block";
-// }
-// showContent();
-// document.querySelectorAll('.questionContainer').addEventListener("click", showContent);
-//
-// function showContent() {
-//     document.querySelectorAll('.content').style.display = "block";
-// }
-
-// const showContent = () => {
-//     // if (content.style.display === "none") {
-//     //     return content.style.display = "block";
-//     // }
-//    return alert("clicked")
-// }
-//
-// toggleQuestion.addEventListener("click", (showContent));
-
-// let toggle = document.querySelector('.questionContainer')
-// toggle.addEventListener('click', () => {
-//
-// })
+questions.forEach((question) =>{
+    const button = question.querySelector(".questionToggle");
+    const answer = question.querySelector(".answer");
+    button.addEventListener("click", () => {
+        questions.forEach((item) => {
+            if (item !== question) {
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle("show-text");
+    });
+    answer.addEventListener("click", () => {
+        questions.forEach((item) => {
+            if (item !== question) {
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle("show-text");
+    });
+});
